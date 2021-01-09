ef5999b80562f2bb2d5e510f98b10193666db55f
Philipp Badenhoop <philipp.badenhoop@hellocodeblog.com>
2021-01-09 15:41:57 +0100

    Implement it

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   jpegtran: { progressive: true, arithmetic: false },
-  gifsicle: { optimizationLevel: 2, interlaced: false, colors: 10 },
+  gifsicle: { optimizationLevel: 2, interlaced: false, colors: 20 },
   pngquant: { quality: [0.25, 0.5] },
   svgo: {
       plugins: [{ removeViewBox: false }, { cleanupIDs: true }],
--- www/index.js
+++ www/index.js
@@ -1,6 +1,27 @@
-import "./sass/main.scss"
-import $ from "jquery"
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+import './sass/main.scss'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import $ from 'jquery'
 
-console.log('hello world')
+gsap.registerPlugin(ScrollTrigger); 
+
+const fadeInElements = gsap.utils.toArray('.fade-in')
+fadeInElements.forEach(element =>
+  gsap.from(element, {
+  scrollTrigger: {
+      trigger: element,
+      start: 'top center',
+  },
+  y: 50,
+  opacity: 0.0,
+  duration: 1.5,
+}))
+
+$('.download-btn').on('click', function() {
+  console.log('Click')
+  $('html,body').animate({
+      scrollTop: $('.download').offset().top
+    },
+    'fast',
+  )
+})
