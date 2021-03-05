18290643f18fcb3548357e271c89d4e286a1e067
Philipp Badenhoop <philipp.badenhoop@hellocodeblog.com>
2021-03-05 23:38:30 +0100

    -

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   jpegtran: { progressive: true, arithmetic: false },
-  gifsicle: { optimizationLevel: 10, interlaced: true, colors: 30 },
+  gifsicle: { optimizationLevel: 10, interlaced: true, colors: 50 },
   pngquant: { quality: [0.25, 0.5] },
   svgo: {
       plugins: [{ removeViewBox: false }, { cleanupIDs: true }],
